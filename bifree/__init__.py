78fa45cb0f42ce09caaebe42c099148c824376a3
Diego Prada <prada.gracia@gmail.com>
2022-02-16 18:50:46 -0600

    Adding PyUnitWizard

--- bifree/__init__.py
+++ bifree/__init__.py
@@ -14,5 +14,7 @@ __documentation_web__ = 'https://www.uibcdf.org/BiFrEE'
 __github_web__ = 'https://github.com/uibcdf/BiFrEE'
 __github_issues_web__ = __github_web__ + '/issues'
 
+from ._pyunitwizard import puw as puw
+
 from .test import greetings
 
